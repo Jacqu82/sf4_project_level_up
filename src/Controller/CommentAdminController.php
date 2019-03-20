f146f116dcd2485f405a2e8ab7c844968394cf88
Jacek Wesołowski <jacqu25@yahoo.com>
2019-03-20 22:13:18 +0100

    twig blocks

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/comment", name="comment_admin")
+     */
+    public function index()
+    {
+        return $this->render('comment_admin/index.html.twig', [
+        ]);
+    }
+}
